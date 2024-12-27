0f6d1d3f348cc3dc4960eb28f40f38aeae8fb820
rjchiba <ryuji.chiba.5185@gmail.com>
2024-12-28 08:44:57 +0900

    resolve #1 - set highlight to running ControllerItem
    to achive above,
    - replace play/pause button to ReactComponent using svg from text
    - add new class handling the color of ControllerItem for both light-mode and dark-mode
    
    in addition
    - remove favicon

---
+++ src/app/atom/Icons/PlayerPlay.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = ({fill, stroke}) => {
+
+  return (
+    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24">
+    <path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" />
+    </svg>
+  );
+}
+export default App;
